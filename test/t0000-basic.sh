106b21e0d147d8f8a77176966f556556f6d094b4
Gaël-Ian Havard <gaelian.havard@gmail.com>
2017-08-03 17:03:00 +1000

    Switch indentation to tabs instead of spaces
    As bash Heredoc syntax works with tabs but not with spaces.

--- test/t0000-basic.sh
+++ test/t0000-basic.sh
@@ -7,15 +7,15 @@ test_description="Show basic features of chapecron"
 CHAPECRON=../chapecron
 
 test_expect_success "Output nothing on success" '
-    test -z $($CHAPECRON date)
+	test -z $($CHAPECRON date)
 '
 
 test_expect_success "Output nothing on standard output on failure" '
-    test -z $(2>/dev/null $CHAPECRON date -w)
+	test -z $(2>/dev/null $CHAPECRON date -w)
 '
 
 test_expect_success "Output something on standard error output on failure" '
-    test -n $(1>/dev/null $CHAPECRON date -w)
+	test -n $(1>/dev/null $CHAPECRON date -w)
 '
 
 test_done
