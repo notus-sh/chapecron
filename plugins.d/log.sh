e427a59bb1ab9654b1dfb89beb8dd33ccedf8b76
Gaël-Ian Havard <gaelian.havard@gmail.com>
2017-09-04 16:13:39 +1000

    Refactor the logging functions
    Rename logging functions to their own namespace 'log' and add a mecanism
    to redirect standard and error logs to temporary files when one of the
    log function is called from inside the captured part of the stack.

--- plugins.d/log.sh
+++ plugins.d/log.sh
@@ -5,6 +5,6 @@ chapecron::log() {
 		utils::fail $EX_CONFIG \
 								"Missing configuration: log.path"
 
-	utils::debug "Command's output will be copied to ${CONFIG['log.path']}"
+	log::debug "Command's output will be copied to ${CONFIG['log.path']}"
 	stack::next | tee --append "${CONFIG['log.path']}"
 }
